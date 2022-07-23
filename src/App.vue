b4c76d3a74e369d48aadf60fa12b1dfc0d1904ee
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-24 04:47:55 +0530

    added basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      app
+      color="#2E2E2E"
+      dark
+      v-model="drawer"
+      :mini-variant="true"
+      permanent
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-icon color="#4EAC6D">mdi-message-text</v-icon>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* width */
+::-webkit-scrollbar {
+  width: 8px;
 }
 
-#nav {
-  padding: 30px;
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
---
+++ src/views/ChatMain.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main white--text">
+    <div class="detailSection">asd</div>
+
+    <div class="chatSection">asd</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.main {
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+}
+
+.detailSection {
+  height: 100vh;
+  width: 318px;
+  background-color: #262626;
+}
+
+.chatSection {
+  height: 100vh;
+  flex-grow: 1;
+  background-color: #2e2e2e;
+}
+
+
+</style>
