3867029ae0df66e6c528434e1732aaf8926fbca0
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-08-21 14:02:25 +0530

    added image file send feature

---
+++ src/components/ChatComponents/Types/ImageChatComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pa-3 ml-10" :style=" belong == 'me' ? 'background-color: rgba(53, 75, 60, 0.5)' : 'background-color: #383838' ">
+      <p :style="belong == 'me' ? 'text-align:right' : 'text-align:left' ">
+        <img :src="item.message.split('|')[0]" width="300" />
+      </p>
+      <label :style="belong == 'me' ? 'text-align:right' : 'text-align:left' " v-if="item.message.split('|')[1]">{{item.message.split('|')[1] && item.message.split('|')[1] }}</label>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+    props : ['item' , 'belong']
+
+}
+</script>
--- src/components/Dialogs/Chat/UploadFile.vue
+++ src/components/Dialogs/Chat/UploadFile.vue
@@ -1,14 +1,12 @@
 <template>
   <v-dialog persistent max-width="500" v-model="dialog">
-
     <template v-slot:activator="{ on, attrs }">
-      <v-btn v-bind="attrs" v-on="on" color="success" outlined >
+      <v-btn v-bind="attrs" v-on="on" color="success" outlined>
         <v-icon color="success">mdi-paperclip</v-icon>
       </v-btn>
     </template>
 
     <v-card color="#262626" dark>
-
       <v-toolbar class="transparent" elevation="0" color="success" dark>
         <v-toolbar-title class="subheading">Upload Files</v-toolbar-title>
         <v-spacer></v-spacer>
@@ -19,76 +17,167 @@
 
       <v-card-text @dragover.prevent @drop.prevent class="pa-5">
 
-        <div @drop="dragFile" @click="handleOpenFileDialog" style=" width: 100%; height: 200px; cursor: pointer; border: 1px dashed white; display: flex; justify-content: center; align-items: center;">
-            <h3>{{ selectedFile == null ? 'Drag your files here or click to upload' : selectedFile.name }}</h3>
+        <v-alert v-model="errorShow" class="mb-5" dense outlined type="error">
+          {{ error }}
+        </v-alert>
+
+        <div
+          @drop="dragFile"
+          @click="handleOpenFileDialog"
+          style="
+            width: 100%;
+            height: 200px;
+            cursor: pointer;
+            border: 1px dashed white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          <h3>
+            {{
+              selectedFile == null
+                ? "Drag your files here or click to upload"
+                : selectedFile.name
+            }}
+          </h3>
         </div>
 
         <input
-            ref="fileField"
-            style="display: none"
-            type="file"
-            @change="handleFileAdded"
+          ref="fileField"
+          style="display: none"
+          type="file"
+          @change="handleFileAdded"
         />
 
         <div class="d-flex mt-4">
-            <v-text-field
-                label="Type a message..."
-                hide-details
-                style="width: 100%"
-                dense
-                dark
-                outlined
-                class="my-auto"
-            />
+          <v-text-field
+            label="Type a message..."
+            hide-details
+            style="width: 100%"
+            dense
+            dark
+            outlined
+            class="my-auto"
+            v-model="message"
+          />
 
-            <div class="d-flex ml-3 my-auto">
-                <v-btn style="margin-left : 5px;" color="success">
-                    <v-icon>mdi-send</v-icon>
-                </v-btn>
-            </div>
-
+          <div class="d-flex ml-3 my-auto">
+            <v-btn
+              type="submit"
+              @click="sendMessage"
+              style="margin-left: 5px"
+              color="success"
+            >
+              <v-icon>mdi-send</v-icon>
+            </v-btn>
+          </div>
         </div>
-      
-      </v-card-text>
 
-      
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 
+import Chat from '../../../Repository/Chat';
 
 export default {
-
   data() {
     return {
       dialog: false,
-      selectedFile : null
-    };
+      selectedFile: null,
 
+      message : '',
+
+      errorShow : false,
+      error : ""
+
+    };
   },
 
-  methods : {
-
-    async close(){
+  methods: {
+    async close() {
       this.dialog = false;
+      this.message = '';
+      this.selectedFile = null;
+
+      this.errorShow = false;
+      this.error = "";
+
     },
 
-    handleFileAdded(e){
-        this.selectedFile = e.target.files[0];
+    handleFileAdded(e) {
+      this.selectedFile = e.target.files[0];
     },
 
-    dragFile(e){
-        e.preventDefault();
-        this.selectedFile = e.dataTransfer.files[0];
+    dragFile(e) {
+      e.preventDefault();
+      this.selectedFile = e.dataTransfer.files[0];
     },
 
-    handleOpenFileDialog(){
-        this.$refs.fileField.click();
-    }
+    handleOpenFileDialog() {
+      this.$refs.fileField.click();
+    },
 
-  }
+    async sendMessage() {
 
+      this.errorShow = false;
+      this.error = "";
+
+      if(this.selectedFile == null){
+        this.errorShow = true;
+        this.error = "Please select a file to upload";
+        return;
+      }
+
+      try{
+
+        let form = new FormData();
+        form.append('file', this.selectedFile);
+
+        let res = await Chat.uploadFile(form);
+        let _id = Date.now()+'' + this.$store.state.socket.id;
+
+        console.log(res);
+
+        let msg = res.data.file.file_path + "|" + this.message;
+        
+        this.$store.state.socket.emit("private:sendMessage" , {
+          message_type : res.data.file.file_type,
+          message: msg,
+          user_from: this.$store.state.currentUser,
+          user_to: this.$store.state.chat.chat.activeProfile,
+          date : new Date(),
+          room_id: this.$store.state.chat.chat.activeProfile.room_id,
+          private_id : _id
+        });
+
+        // // add to store
+        this.$store.commit('addChatMessage' , {
+          message_type : res.data.file.file_type,
+          message: msg,
+          user_from: this.$store.state.currentUser,
+          user_to: this.$store.state.chat.chat.activeProfile,
+          from_user_id : this.$store.state.currentUser.id,
+          date : new Date(),
+          room_id: this.$store.state.chat.chat.activeProfile.room_id,
+          id : _id,
+          is_read : false
+        });
+
+        this.message = '';
+        this.selectedFile = null;
+
+        this.close();
+
+      }catch(err){
+        this.errorShow = false;
+        this.error = err.response.data.message;
+      }
+
+    },
+  },
 };
 </script>
