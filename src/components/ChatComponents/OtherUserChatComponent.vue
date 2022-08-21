698e7583104c1473d2be6bec2dd1f7a7bf397100
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-08-21 18:25:49 +0530

    added small changes

--- src/components/ChatComponents/OtherUserChatComponent.vue
+++ src/components/ChatComponents/OtherUserChatComponent.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="mt-2" style="max-width: 50%; min-width: auto">
-
-    <div v-if="item.message_type == 'text' " class="pa-3 ml-10" style="background-color: #383838">
+    <div
+      v-if="item.message_type == 'text'"
+      class="pa-3 ml-10"
+      style="background-color: #383838"
+    >
       <p>
         {{ item.message }}
       </p>
     </div>
 
-    <ImageChatComponent 
-      v-else-if="item.message_type == 'image' " 
+    <ImageChatComponent
+      v-else-if="item.message_type == 'image'"
       belong="other"
       :item="item"
     />
 
     <FileChatComponent
-      v-else-if="item.message_type == 'file' || item.message_type == 'video' " 
+      v-else-if="item.message_type == 'file' || item.message_type == 'video'"
       belong="other"
       :item="item"
     />
 
     <div class="mt-2 d-flex">
       <v-avatar size="30px" class="my-auto">
-        <img
-          :src="profileImage()"
-        />
+        <img :src="profileImage()" />
       </v-avatar>
 
       <label class="my-auto ml-2" style="font-size: 12px">{{
         formatDate(item.createdAt)
       }}</label>
     </div>
-
   </div>
 </template>
 
@@ -40,21 +40,23 @@ import ImageChatComponent from "./Types/ImageChatComponent.vue";
 import FileChatComponent from "./Types/FileChatComponent.vue";
 
 export default {
-    props: ["item"],
-    methods: {
-        formatDate(date) {
-            // if date is today then return time
-            if (moment(date).isSame(moment(), "day")) {
-                return moment(date).format("h:mm a");
-            }
-            else {
-                return moment(date).format("MMM DD");
-            }
-        },
-        profileImage() {
-            return process.env.VUE_APP_SOCKET_URL + this.$store.state.chat.chat.activeProfile.user.profile_image;
-        }
+  props: ["item"],
+  methods: {
+    formatDate(date) {
+      // if date is today then return time
+      if (moment(date).isSame(moment(), "day")) {
+        return moment(date).format("h:mm a");
+      } else {
+        return moment(date).format("MMM DD h:mm a");
+      }
     },
-    components: { ImageChatComponent, FileChatComponent }
+    profileImage() {
+      return (
+        process.env.VUE_APP_SOCKET_URL +
+        this.$store.state.chat.chat.activeProfile.user.profile_image
+      );
+    },
+  },
+  components: { ImageChatComponent, FileChatComponent },
 };
 </script>
