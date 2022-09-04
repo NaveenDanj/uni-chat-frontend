531bf7021323ea6fe7bfe5c5aecfe88fce5c57f8
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-09-04 21:23:36 +0530

    added menu option to contact

---
+++ src/components/SideSection/Contact/ContactItem.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-list-item @click="handleSetActiveChat" denselink style="font-size: 10px">
+    <v-list-item-avatar size="30px">
+      <img :src="formatImage(contact)" />
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>{{ contact.contact_name }}</v-list-item-title>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <!-- <v-btn icon x-small>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn> -->
+
+      <v-menu
+        style="background-color: #262626"
+        dark
+        left
+        :offset-y="true"
+        min-width="170"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon x-small v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item link>
+            <v-list-item-title class="my-auto">Edit</v-list-item-title>
+
+            <v-list-item-icon class="my-auto">
+              <v-icon dark class="white--text" x-small>mdi-pencil</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-title class="my-auto">Block</v-list-item-title>
+
+            <v-list-item-icon class="my-auto">
+              <v-icon dark class="white--text" x-small>mdi-cancel</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-title class="my-auto">Remove</v-list-item-title>
+
+            <v-list-item-icon class="my-auto">
+              <v-icon dark class="white--text" x-small>mdi-trash-can</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+
+        </v-list>
+      </v-menu>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script>
+import Chat from "../../../Repository/Chat";
+
+export default {
+  props: ["contact"],
+
+  methods: {
+    formatImage(item) {
+      return process.env.VUE_APP_SOCKET_URL + item.user.profile_image;
+    },
+
+    async handleSetActiveChat() {
+      let item = this.contact;
+
+      if (this.$store.state.chat.chat.activeProfile) {
+        if (this.$store.state.chat.chat.activeProfile.user.id == item.user.id) {
+          return;
+        }
+      }
+
+      this.$store.commit("setChatActiveProfile", item);
+      this.$store.state.socket.emit("private:join", item);
+      this.$store.commit("setChatMessages", []);
+
+      // load the previous messages
+      try {
+        let messages = await Chat.loadUserChats(
+          item.contact_id,
+          item.room_id,
+          1
+        );
+        let array = messages.data.messages.reverse();
+        this.$store.commit("setChatMessages", array);
+        let private_ids = [];
+
+        for (let i = 0; i < array.length; i++) {
+          if (
+            array[i].is_read == false &&
+            array[i].to_user_id == this.$store.state.currentUser.id
+          ) {
+            private_ids.push(array[i].private_id);
+          }
+        }
+
+        this.$store.state.socket.emit("private:readMessage", {
+          messages: private_ids,
+          room_id: item.room_id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
--- src/views/ChatFlow/Contact.vue
+++ src/views/ChatFlow/Contact.vue
@@ -24,119 +24,102 @@
     </div>
 
     <div>
-
-
-
-      <div v-for="(item , index) in charList" :key="index">
-
+      <div v-for="(item, index) in charList" :key="index">
         <div class="d-flex">
-          <label style="font-size: 12px; color: #4eac6d" class="mr-5">{{item}}</label>
+          <label style="font-size: 12px; color: #4eac6d" class="mr-5">{{
+            item
+          }}</label>
           <v-divider class="my-auto" dark />
         </div>
 
         <v-list dense dark color="#262626">
-          <v-list-item
-            v-for="(contact, index2) in contactList[item] "
+          <ContactItem
+            v-for="(contact, index2) in contactList[item]"
             :key="index2"
-            dense
-            link
-            style="font-size: 10px"
-          >
-            <v-list-item-avatar size="30px">
-              <img
-                :src="formatImage(contact)"
-              />
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ contact.contact_name }}</v-list-item-title>
-            </v-list-item-content>
-
-            <v-list-item-action>
-                <v-btn icon x-small>
-                    <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-            </v-list-item-action>
-
-          </v-list-item>
+            :contact="contact"
+          />
         </v-list>
-
       </div>
-
     </div>
-
   </div>
-
 </template>
 
 <script>
-import Contact from '../../Repository/Contact';
-import AddContact from '../../components/Dialogs/AddContact.vue';
+import Contact from "../../Repository/Contact";
+import AddContact from "../../components/Dialogs/AddContact.vue";
+import ContactItem from "../../components/SideSection/Contact/ContactItem.vue";
 
 export default {
-    async created() {
-        this.loadContacts();
-    },
-    methods: {
-        async loadContacts() {
-            this.contactList = {};
-            this.charList = [];
-            let contacts = await Contact.getAllContacts();
-            contacts = contacts.data.contacts;
-            contacts = this.sortContact(contacts);
-            for (let i = 0; i < contacts.length; i++) {
-                let fChar = contacts[i].contact_name.toUpperCase()[0];
-                if (this.contactList[fChar] == undefined) {
-                    this.contactList[fChar] = [];
-                }
-                this.contactList[fChar].push(contacts[i]);
-            }
-            this.contactListCopy = Object.assign({}, this.contactList);
-            this.charList = Object.keys(this.contactList);
-        },
-        sortContact(contacts) {
-            return contacts.sort(function (a, b) {
-                var nameA = a.contact_name.toUpperCase();
-                var nameB = b.contact_name.toUpperCase();
-                if (nameA < nameB) {
-                    return -1;
-                }
-                if (nameA > nameB) {
-                    return 1;
-                }
-                return 0;
-            });
-        },
-        formatImage(item) {
-            console.log("ran");
-            return process.env.VUE_APP_SOCKET_URL + item.user.profile_image;
-        },
-        handleSearch() {
-            let char = this.searchString.toUpperCase()[0];
-            if (this.searchString == "") {
-                this.loadContacts();
-                return;
-            }
-            if (this.contactList[char] == undefined) {
-                return;
-            }
-            this.contactList[char] = this.contactListCopy[char].filter((item) => item.contact_name.toLowerCase().includes(this.searchString.toLowerCase()));
-            for (let i = 0; i < Object.keys(this.contactList).length; i++) {
-                if (Object.keys(this.contactList)[i] != char) {
-                    delete this.contactList[Object.keys(this.contactList)[i]];
-                }
-            }
+  async created() {
+    this.loadContacts();
+  },
+  methods: {
+    async loadContacts() {
+      this.contactList = {};
+      this.charList = [];
+      let contacts = await Contact.getAllContacts();
+      contacts = contacts.data.contacts;
+      contacts = this.sortContact(contacts);
+      for (let i = 0; i < contacts.length; i++) {
+        let fChar = contacts[i].contact_name.toUpperCase()[0];
+        if (this.contactList[fChar] == undefined) {
+          this.contactList[fChar] = [];
         }
+        this.contactList[fChar].push(contacts[i]);
+      }
+      this.contactListCopy = Object.assign({}, this.contactList);
+      this.charList = Object.keys(this.contactList);
     },
-    data() {
-        return {
-            contactList: {},
-            charList: [],
-            searchString: "",
-            contactListCopy: {},
-            drawer: true,
-        };
+
+    sortContact(contacts) {
+      return contacts.sort(function (a, b) {
+        var nameA = a.contact_name.toUpperCase();
+        var nameB = b.contact_name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
     },
-    components: { AddContact }
+
+    formatImage(item) {
+      return process.env.VUE_APP_SOCKET_URL + item.user.profile_image;
+    },
+
+    handleSearch() {
+      let char = this.searchString.toUpperCase()[0];
+      if (this.searchString == "") {
+        this.loadContacts();
+        return;
+      }
+      if (this.contactList[char] == undefined) {
+        return;
+      }
+      this.contactList[char] = this.contactListCopy[char].filter((item) =>
+        item.contact_name
+          .toLowerCase()
+          .includes(this.searchString.toLowerCase())
+      );
+      for (let i = 0; i < Object.keys(this.contactList).length; i++) {
+        if (Object.keys(this.contactList)[i] != char) {
+          delete this.contactList[Object.keys(this.contactList)[i]];
+        }
+      }
+    },
+
+  },
+  data() {
+    return {
+      contactList: {},
+      charList: [],
+      searchString: "",
+      contactListCopy: {},
+      drawer: true,
+    };
+  },
+  components: { AddContact, ContactItem },
 };
 </script>
