8aacec310f6fe1f7a5e0b7962a762f1d33f2f3f6
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-09-06 00:22:08 +0530

    added bookmark repo and added bookmark item

---
+++ src/components/Bookmark/BookmarkItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-item class="mt-2">
+    <v-list-item-avatar>
+      <v-icon color="success">mdi-file-document-outline</v-icon>
+    </v-list-item-avatar>
+
+    <v-list-item-content>
+      <v-list-item-title>
+        <div class="d-flex justify-space-between">
+          <label class="my-auto" style="font-size: 13px">
+            <span style="color: #9ba2a9">design-phase-1...</span><br />
+            <span style="color: #9ba2a9">12.5MB</span>
+          </label>
+
+          <v-btn x-small icon fab>
+            <v-icon style="color: #9ba2a9">mdi-dots-vertical</v-icon>
+          </v-btn>
+        </div>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
